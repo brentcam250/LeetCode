36734d4b709b23d34b5063a463b0eb02791c5681
Brent <bjcameron@gmail.com>
2020-04-17 09:56:50 -0600

    added some details in stepsToZero about the question and my solution

--- stepsToZero.js
+++ stepsToZero.js
@@ -1,3 +1,13 @@
+/*
+Question:
+Given a non-negative integer num, return the number of steps to
+reduce it to zero. If the current number is even, you have to
+divide it by 2, otherwise, you have to subtract 1 from it.
+
+
+
+*/
+
 /**
  * @param {number} num
  * @return {number}
@@ -16,7 +26,7 @@ var numberOfSteps  = function(num) {
     }
 };
 
-//call this func 
+//call this func
 //may be able to just recursivly call the first function
 function steps(number){
     if(number %2 ===0){
@@ -28,3 +38,12 @@ function steps(number){
         return 0;
     }
 }
+
+/*
+Results:
+Runtime: 60 ms, faster than 31.33% of JavaScript online
+submissions for Number of Steps to Reduce a Number to Zero.
+Memory Usage: 33.8 MB, less than 100.00% of JavaScript online
+submissions for Number of Steps to Reduce a Number to Zero.
+
+*/
